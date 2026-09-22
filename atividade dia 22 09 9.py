387c369a4d50ac07fe793867b97ad2955a9afa8a
filhoslima37-rgb <filhoslima37@gmail.com>
2026-09-22 15:03:05 -0400

    Add files via upload

---
+++ atividade dia 22 09 9.py
@@ -0,0 +1,11 @@
+raio = float(input("Digite o raio do cilindro: "))
+altura = float(input("Digite a altura do cilindro: "))
+
+pi = 3.14
+area_base = pi * raio ** 2
+volume = area_base * altura
+
+print("O volume do cilindro é:", volume)
+
+
+
