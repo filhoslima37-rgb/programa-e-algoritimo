f30cc8be34933dee38b1be0542d6d5aa239d9453
filhoslima37-rgb <filhoslima37@gmail.com>
2026-09-22 15:02:04 -0400

    Add files via upload

---
+++ atividade dia 22 09 6.py
@@ -0,0 +1,12 @@
+valor_a = int(input("Digite o valor de A: "))
+valor_b = int(input("Digite o valor de B: "))
+valor_c = int(input("Digite o valor de C: "))
+valor_d = int(input("Digite o valor de D: "))
+
+media = (valor_a * 3 + valor_b * 4 + valor_c * 2 + valor_d * 5) / 14
+
+print("A média é:", media)
+
+
+
+
