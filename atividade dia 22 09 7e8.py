70db9f053586391ba5333fbb3bbe9db6c1d77c78
filhoslima37-rgb <filhoslima37@gmail.com>
2026-09-22 15:02:46 -0400

    Add files via upload

---
+++ atividade dia 22 09 7e8.py
@@ -0,0 +1,12 @@
+import math
+
+lado1 = float(input("Digite o primeiro lado: "))
+lado2 = float(input("Digite o segundo lado: "))
+
+hipotenusa = math.hypot(lado1, lado2)
+
+print("A hipotenusa do triângulo é:", hipotenusa)
+
+
+
+
