763b6622727fcf1394795d54ce766a56262f8cc7
filhoslima37-rgb <filhoslima37@gmail.com>
2026-09-22 14:59:24 -0400

    Add files via upload

---
+++ atividade dia 22 09 1.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+soma = nota1 + nota2 + nota3
+
+print("A soma das três notas é:", soma)
